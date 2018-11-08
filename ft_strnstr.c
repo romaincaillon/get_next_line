58ef0481d65331a994e45831d2f11b631ae6dd42
Romain CAILLON <romain.caillon91@gmail.com>
2018-11-08 15:44:13 +0100

    add function ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcaillon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/08 15:01:30 by rcaillon          #+#    #+#             */
+/*   Updated: 2018/11/08 15:43:31 by rcaillon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t  start;
+	size_t	i;
+	int		found;
+
+	if (*needle == '\0')
+		return ((char *)haystack);
+	start = 0;
+	while (haystack[start] != '\0' && start < len)
+	{
+		i = 0;
+		found = 1;
+		while (needle[i] != '\0' && found && start + i < len)
+		{
+			if (haystack[start + i] != needle[i])
+				found = 0;
+			i++;
+		}
+		if (found && needle[i] == '\0')
+			return ((char *)haystack + start);
+		start++;
+	}
+	return (NULL);
+}
