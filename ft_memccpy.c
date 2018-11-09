e70b5a7b4882111010b9d4c8b73622a1d7334fbc
Romain CAILLON <romain.caillon91@gmail.com>
2018-11-09 21:06:50 +0100

    cast to unsigned char in memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: rcaillon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 14:23:31 by rcaillon          #+#    #+#             */
-/*   Updated: 2018/11/08 16:39:50 by rcaillon         ###   ########.fr       */
+/*   Updated: 2018/11/09 21:06:32 by rcaillon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		((char *)dst)[i] = ((char *)src)[i];
-		if (((char *)src)[i] == (unsigned char)c)
+		((unsigned char *)dst)[i] = ((char *)src)[i];
+		if (((unsigned char *)src)[i] == (unsigned char)c)
 			return (dst + i + 1);
 		i++;
 	}
