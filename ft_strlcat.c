c58e8024376e05aafc6589bf3326de580d43ae85
Romain CAILLON <romain.caillon91@gmail.com>
2018-11-09 21:16:32 +0100

    strcat now work with a size of 0

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rcaillon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 14:25:43 by rcaillon          #+#    #+#             */
-/*   Updated: 2018/11/08 16:42:27 by rcaillon         ###   ########.fr       */
+/*   Updated: 2018/11/09 21:15:45 by rcaillon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t i;
 
 	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
 	i = 0;
 	while (i < size - 1 && dst[i] != '\0')
 		i++;
