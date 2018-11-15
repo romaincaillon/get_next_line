8478741e94fd8e9face138d4c2c01ad76a355bdc
Romain CAILLON <romain.caillon91@gmail.com>
2018-11-15 16:20:59 +0100

    fix

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,19 @@
 /*   By: rcaillon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 09:34:59 by rcaillon          #+#    #+#             */
-/*   Updated: 2018/11/15 15:54:07 by rcaillon         ###   ########.fr       */
+/*   Updated: 2018/11/15 16:19:21 by rcaillon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft.h"
+# include "libft/libft.h"
 # include <fcntl.h>
 # include <sys/types.h>
 # include <sys/uio.h>
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 8
 
 int		get_next_line(const int fd, char **line);
 
